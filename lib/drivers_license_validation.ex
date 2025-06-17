47e5b5624b45eaed99454303e8f25fb23d9ec927
sharath <hosur.sharath@gmail.com>
2025-06-17 12:34:32 +0530

    Initial commit

---
+++ lib/drivers_license_validation.ex
@@ -0,0 +1,18 @@
+defmodule DriversLicenseValidation do
+  @moduledoc """
+  Documentation for `DriversLicenseValidation`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> DriversLicenseValidation.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/drivers_license_validation_test.exs
@@ -0,0 +1,8 @@
+defmodule DriversLicenseValidationTest do
+  use ExUnit.Case
+  doctest DriversLicenseValidation
+
+  test "greets the world" do
+    assert DriversLicenseValidation.hello() == :world
+  end
+end
